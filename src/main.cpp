c0a7b53912793589cc390f85a8146cdad97b5fac
Michel Hardenberg <MHardenberg@proton.me>
2024-01-16 12:57:23 +0100

    quick file read

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include "game.h"
-
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 
 int main() {
+    
+    string line;
+    ifstream file;
+    file.open("map/test.map");
+    if (file.is_open()){
+        while(getline(file, line)){
+            cout << line << endl;
+        }
+            file.close();
+    }
+
     Game game;
     game.run();
-
     return 0;
 
 }
