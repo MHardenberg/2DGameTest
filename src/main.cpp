8399c42644462767c5f74007c1ec6f11bb270bd2
Michel Hardenberg <MHardenberg@proton.me>
2024-01-18 09:03:55 +0100

    rewrite to avoid mem leaks

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include "game.h"
-#include "gamemap.h"
 using namespace std;
 
 
 int main() {
     
-    GameMap map;
-    map.fromFile("map/test.map");
-    map.debugPrint();
+    cout << "test!" << endl;
 
-
-    Game game;
-    game.run();
     return 0;
 
 }
