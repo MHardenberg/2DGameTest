e21b5438c651a6baad9dc6900d770f2750d4c587
Michel Hardenberg <MHardenberg@proton.me>
2024-03-26 20:31:26 +0100

    init window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
+#include <SDL2/SDL_events.h>
 #include <iostream>
+#include <SDL2/SDL.h>
+
+
+struct Window {
+    Window(){
+        SDL_Init(SDL_INIT_VIDEO);
+        SDLWindow = SDL_CreateWindow("Hello Window", 0, 0, 500, 300, 0
+    );
+
+    SDLWindowSurface = SDL_GetWindowSurface(SDLWindow);
+
+    SDL_FillRect(SDLWindowSurface, nullptr,
+            SDL_MapRGB(SDLWindowSurface->format, 40, 40, 40)
+    );
+  }
+
+  void RenderFrame() {
+    SDL_UpdateWindowSurface(SDLWindow);
+  }
+
+private:
+  SDL_Window* SDLWindow { nullptr };
+  SDL_Surface* SDLWindowSurface { nullptr };
+};
 
 
 int main(){
-    std::cout << "Hello world!" << std::endl;
+    Window window;
+    SDL_Event event;
+
+    while (true) {
+        SDL_PollEvent(&event);
+        window.RenderFrame();
+    }
 
     return 0;
 }
